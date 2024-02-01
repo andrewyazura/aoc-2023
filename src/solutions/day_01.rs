87ee3b4db80c774dcb93976b22c448406b13ed93
Andrew Yatsura <andrewyazura203@proton.me>
2024-02-01 04:50:23 +0100

    add day 1 part 1 solution

--- src/solutions/day_01.rs
+++ src/solutions/day_01.rs
@@ -2,16 +2,36 @@ use crate::solution;
 
 pub struct Solution;
 
-impl solution::Solution<1, Vec<String>, i128> for Solution {
-    fn process_input(&self, input: &str) -> Vec<String> {
-        input.split('\n').map(|s| s.to_owned()).collect()
+impl solution::Solution<1, Vec<String>, u32> for Solution {
+    fn process_input(&self, input: String) -> Vec<String> {
+        input
+            .split('\n')
+            .filter(|s| !s.is_empty())
+            .map(|s| s.to_owned())
+            .collect()
     }
 
-    fn part_1(&self, input: &Vec<String>) -> i128 {
-        0
+    fn part_1(&self, input: &Vec<String>) -> u32 {
+        input
+            .iter()
+            .map(|calibration_value| {
+                let mut numbers = calibration_value
+                    .chars()
+                    .filter(|c| c.is_numeric())
+                    .map(|c| c.to_digit(10).unwrap_or_else(|| panic!("Unexpected error")));
+
+                let first = numbers.next().unwrap_or_else(|| {
+                    panic!("No numbers in calibration code {}", &calibration_value)
+                });
+
+                let last = numbers.last().unwrap_or(first);
+
+                format!("{first}{last}").parse::<u32>().unwrap()
+            })
+            .sum()
     }
 
-    fn part_2(&self, input: &Vec<String>) -> i128 {
+    fn part_2(&self, _input: &Vec<String>) -> u32 {
         0
     }
 }

4933cba603bc386a8f64058c109b672aaf08982e
Andrew Yatsura <andrewyazura203@proton.me>
2024-02-01 14:46:33 +0100

    add day 1 part 2

--- src/solutions/day_01.rs
+++ src/solutions/day_01.rs
@@ -31,7 +31,54 @@ impl solution::Solution<1, Vec<String>, u32> for Solution {
             .sum()
     }
 
-    fn part_2(&self, _input: &Vec<String>) -> u32 {
-        0
+    fn part_2(&self, input: &Vec<String>) -> u32 {
+        const NUMBERS: [&str; 9] = [
+            "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+        ];
+
+        input
+            .iter()
+            .map(|calibration_value| {
+                let mut numbers =
+                    calibration_value
+                        .chars()
+                        .enumerate()
+                        .filter_map(|(i, c)| match c {
+                            c if c.is_numeric() => c.to_digit(10),
+                            _ => {
+                                for n in NUMBERS {
+                                    if calibration_value[i..].starts_with(n) {
+                                        return text_to_int(&calibration_value[i..i + n.len()]);
+                                    }
+                                }
+
+                                return None;
+                            }
+                        });
+
+                let first = numbers.next().unwrap_or_else(|| {
+                    panic!("No numbers in calibration code {}", &calibration_value)
+                });
+
+                let last = numbers.last().unwrap_or(first);
+
+                format!("{first}{last}").parse::<u32>().unwrap()
+            })
+            .sum()
+    }
+}
+
+fn text_to_int(text: &str) -> Option<u32> {
+    match text {
+        "one" => Some(1),
+        "two" => Some(2),
+        "three" => Some(3),
+        "four" => Some(4),
+        "five" => Some(5),
+        "six" => Some(6),
+        "seven" => Some(7),
+        "eight" => Some(8),
+        "nine" => Some(9),
+        _ => None,
     }
 }

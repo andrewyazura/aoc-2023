86a8be435f68f283fa73c1c89368de82d2d6e035
Andrew Yatsura <andrewyazura203@proton.me>
2024-02-10 06:46:54 +0100

    restructure day 2 and add part 2 solution

--- src/solutions/day_02.rs
+++ src/solutions/day_02.rs
@@ -4,15 +4,11 @@ use lazy_static::lazy_static;
 use regex::Regex;
 use std::str::FromStr;
 
-struct Set {
-    red: u32,
-    green: u32,
-    blue: u32,
-}
-
 struct Game {
     id: u32,
-    sets: Vec<Set>,
+    reds: Vec<u32>,
+    greens: Vec<u32>,
+    blues: Vec<u32>,
 }
 
 lazy_static! {
@@ -31,27 +27,33 @@ impl FromStr for Game {
         let id: u32 = game_captures[1].parse().unwrap();
         let sets_str = &game_captures[2];
 
-        let sets = sets_str
-            .split(";")
-            .map(|set_str| {
-                let (mut red, mut green, mut blue) = (0, 0, 0);
+        let (mut reds, mut blues, mut greens) = (vec![], vec![], vec![]);
 
-                for color_capture in COLOR_REGEX.captures_iter(set_str) {
-                    let quantity: u32 = color_capture[1].parse().unwrap_or(0);
+        for set in sets_str.split(";") {
+            let (mut red, mut green, mut blue) = (0, 0, 0);
 
-                    match &color_capture[2] {
-                        "red" => red = quantity,
-                        "green" => green = quantity,
-                        "blue" => blue = quantity,
-                        _ => (),
-                    }
+            for color_capture in COLOR_REGEX.captures_iter(set) {
+                let quantity: u32 = color_capture[1].parse().unwrap_or(0);
+
+                match &color_capture[2] {
+                    "red" => red = quantity,
+                    "green" => green = quantity,
+                    "blue" => blue = quantity,
+                    _ => (),
                 }
+            }
 
-                Set { red, green, blue }
-            })
-            .collect();
+            reds.push(red);
+            greens.push(green);
+            blues.push(blue);
+        }
 
-        Ok(Game { id, sets })
+        Ok(Game {
+            id,
+            reds,
+            greens,
+            blues,
+        })
     }
 }
 
@@ -79,17 +81,23 @@ impl solution::Solution<2, Vec<String>, u32> for Solution {
         games
             .iter()
             .filter_map(|game| {
-                if game.sets.iter().all(|set| {
-                    if set.red > max_red {
-                        return false;
-                    } else if set.green > max_green {
-                        return false;
-                    } else if set.blue > max_blue {
-                        return false;
-                    }
+                if game
+                    .reds
+                    .iter()
+                    .zip(game.greens.iter())
+                    .zip(game.blues.iter())
+                    .all(|((&r, &g), &b)| {
+                        if r > max_red {
+                            return false;
+                        } else if g > max_green {
+                            return false;
+                        } else if b > max_blue {
+                            return false;
+                        }
 
-                    return true;
-                }) {
+                        return true;
+                    })
+                {
                     return Some(game.id);
                 }
 
@@ -101,6 +109,13 @@ impl solution::Solution<2, Vec<String>, u32> for Solution {
     fn part_2(&self, input: &Vec<String>) -> u32 {
         let games: Vec<Game> = input.iter().map(|s| Game::from_str(s).unwrap()).collect();
 
-        1
+        games
+            .iter()
+            .map(|game| {
+                game.reds.iter().max().unwrap()
+                    * game.greens.iter().max().unwrap()
+                    * game.blues.iter().max().unwrap()
+            })
+            .sum()
     }
 }

2f3bf6c30b357d0f1a85f7bda2768188e4dfdc3a
Andrew Yatsura <andrewyazura203@proton.me>
2024-04-19 19:50:43 +0200

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "aoc-2023 rust env";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system overlays; };
+        overlays = [ (import rust-overlay) ];
+      in with pkgs; {
+        devShells.default = mkShell {
+          buildInputs = [
+            openssl
+            pkg-config
+            (rust-bin.stable.latest.default.override {
+              extensions = [ "rust-src" "rust-analyzer" ];
+            })
+          ];
+
+          shellHook = ''
+            export PATH="$HOME/.cargo/bin:$PATH"
+          '';
+        };
+      });
+}

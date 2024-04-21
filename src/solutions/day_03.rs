c253673d69fdbf8facca2ee1cf007aaf27703a4e
Andrew Yatsura <andrewyazura203@proton.me>
2024-04-21 03:41:03 +0200

    add day 3 part 2

--- src/solutions/day_03.rs
+++ src/solutions/day_03.rs
@@ -1,10 +1,12 @@
 use regex::Regex;
+use std::collections::HashMap;
 
 use crate::solution;
 
 pub struct EngineSchematic {
     numbers: Vec<(u32, usize, usize, usize)>,
     symbols: Vec<(usize, usize)>,
+    gears: Vec<(usize, usize)>,
 }
 
 pub struct Solution;
@@ -58,7 +60,25 @@ impl solution::Solution<3, EngineSchematic, u32> for Solution {
             })
             .collect();
 
-        EngineSchematic { numbers, symbols }
+        let gears = clean_input
+            .iter()
+            .enumerate()
+            .flat_map(|(index, line)| {
+                line.chars()
+                    .enumerate()
+                    .filter_map(|(char_index, char)| match char {
+                        '*' => Some((index, char_index)),
+                        _ => None,
+                    })
+                    .collect::<Vec<(usize, usize)>>()
+            })
+            .collect();
+
+        EngineSchematic {
+            numbers,
+            symbols,
+            gears,
+        }
     }
 
     fn part_1(&self, input: &EngineSchematic) -> u32 {
@@ -79,7 +99,28 @@ impl solution::Solution<3, EngineSchematic, u32> for Solution {
     }
 
     fn part_2(&self, input: &EngineSchematic) -> u32 {
-        1
+        let mut gear_collisions: HashMap<(usize, usize), Vec<u32>> = HashMap::new();
+
+        for (number, line, start, end) in &input.numbers {
+            for line_index in line.saturating_sub(1)..=line.saturating_add(1) {
+                for char_index in start.saturating_sub(1)..end.saturating_add(1) {
+                    if input.symbols.contains(&(line_index, char_index)) {
+                        gear_collisions
+                            .entry((line_index, char_index))
+                            .or_insert_with(Vec::new)
+                            .push(*number);
+                    }
+                }
+            }
+        }
+
+        gear_collisions
+            .iter()
+            .filter_map(|(_, numbers)| match numbers.len() {
+                2 => Some(numbers[0] * numbers[1]),
+                _ => None,
+            })
+            .sum()
     }
 }
 
@@ -106,5 +147,11 @@ mod tests {
     fn test_part_1() {
         let input = SOLUTION.process_input(SAMPLE_INPUT.to_string());
         assert_eq!(SOLUTION.part_1(&input), 4361);
+    }
+
+    #[test]
+    fn test_part_2() {
+        let input = SOLUTION.process_input(SAMPLE_INPUT.to_string());
+        assert_eq!(SOLUTION.part_2(&input), 467835);
     }
 }
